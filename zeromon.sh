ac44f54fa39cd256e4b0a606967cdedc85445a59
Eric OC <eric@ericoc.com>
2018-12-21 14:00:15 +0000

    disable the systemd service once all is said and done

--- zeromon.sh
+++ zeromon.sh
@@ -4,4 +4,5 @@ apt -y install ansible &&
 mkdir /tmp/ansible_{local,remote} &&
 git clone https://github.com/ericoc/zeromon.git /usr/local/src/zeromon &&
 ansible-playbook -i /usr/local/src/zeromon/hosts /usr/local/src/zeromon/setup.yaml &&
-echo >> /root/zabbix_admin_password
+echo >> /root/zabbix_admin_password &&
+systemctl disable zeromon
