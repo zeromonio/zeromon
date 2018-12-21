eada8a53693ee7b9f4a45c83f89aafb50c305125
Eric OC <eric@ericoc.com>
2018-12-20 22:22:43 -0500

    fix inventory to avoid warnings

--- zeromon.sh
+++ zeromon.sh
@@ -2,5 +2,5 @@
 apt update &&
 apt -y install ansible &&
 git clone https://github.com/ericoc/zeromon.git /usr/local/src/zeromon &&
-ansible-playbook /usr/local/src/zeromon/setup.yaml &&
+ansible-playbook -i /usr/local/src/zeromon/hosts /usr/local/src/zeromon/setup.yaml &&
 echo >> /root/zabbix_admin_password
