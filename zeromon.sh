c1186a469a3de5bc38b6bdc3f33e465da69884c1
Eric OC <eric@ericoc.com>
2018-12-21 13:49:52 +0000

    fix up ansible variables to run from systemd better

--- zeromon.sh
+++ zeromon.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt update &&
 apt -y install ansible &&
+mkdir /tmp/ansible_{local,remote} &&
 git clone https://github.com/ericoc/zeromon.git /usr/local/src/zeromon &&
 ansible-playbook -i /usr/local/src/zeromon/hosts /usr/local/src/zeromon/setup.yaml &&
 echo >> /root/zabbix_admin_password
