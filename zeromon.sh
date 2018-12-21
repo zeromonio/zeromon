e482a67b2db42d4175089a9190e919c913be8e3a
Eric OC <eric@ericoc.com>
2018-12-21 03:08:04 +0000

    add bootstrap scripts

---
+++ zeromon.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt update &&
+apt install ansible &&
+git clone https://github.com/ericoc/zeromon.git /usr/local/src/zeromon &&
+ansible-playbook /usr/local/src/zeromon/setup.yaml &&
+echo >> /root/zabbix_admin_password
