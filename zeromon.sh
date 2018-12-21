abecfba18caf7909cf1e83ac52d0144ca4344e97
Eric OC <eric@ericoc.com>
2018-12-21 14:17:20 +0000

    variables were not working right

--- zeromon.sh
+++ zeromon.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+
 apt update &&
 apt -y install ansible &&
+
 mkdir /tmp/ansible_{local,remote} &&
+cat > /root/.ansible.cfg <<'EOF'
+[defaults]
+remote_tmp=/tmp/ansible_remote
+local_tmp=/tmp/ansible_local
+retry_files_enabled=False
+EOF
+
 git clone https://github.com/ericoc/zeromon.git /usr/local/src/zeromon &&
 ansible-playbook -i /usr/local/src/zeromon/hosts /usr/local/src/zeromon/setup.yaml &&
 echo >> /root/zabbix_admin_password &&
+
 systemctl disable zeromon
